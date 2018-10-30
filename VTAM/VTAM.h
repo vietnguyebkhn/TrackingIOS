5cfb3a5e5211d30f4acf59445e1b6183cb5258a7
Xuân Quỳnh <quynhlx@itisol.vn>
2018-10-30 14:18:40 +0700

    Initial Commit

---
+++ VTAM/VTAM.h
@@ -0,0 +1,19 @@
+//
+//  VTAM.h
+//  VTAM
+//
+//  Created by Babie Xcode on 10/30/18.
+//  Copyright © 2018 Quynhbkhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VTAM.
+FOUNDATION_EXPORT double VTAMVersionNumber;
+
+//! Project version string for VTAM.
+FOUNDATION_EXPORT const unsigned char VTAMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VTAM/PublicHeader.h>
+
+
