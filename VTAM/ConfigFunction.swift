d7de336d0df526123f44b863a0bb66b7e5bf7ad8
Fateee98 <luongquanh2204@gmail.com>
2018-11-03 11:44:03 +0700

    update zip func

--- VTAM/ConfigFunction.swift
+++ VTAM/ConfigFunction.swift
@@ -7,9 +7,19 @@
 //
 
 import Foundation
+import Zip
 
 class ConfigFunction {
     var urlBase = "https://vtam-sdk.viettel.com.vn"
+    var mPathFile : URL?
+    var mPathZip : URL?
+    var mFileName = ""
+    
+    init() {
+        let defaults = UserDefaults.standard
+        mFileName = defaults.object(forKey: "fileName") as? String ?? ""
+    }
+    
     //ham get currenttime
     func getCurrentTime() ->  String {
         let date = Date()
@@ -18,12 +28,26 @@ class ConfigFunction {
         let dateString = dateFormatter.string(from: date)
         return dateString
     }
+    
     //Ham log ra file, tra ve duong dan file trong local cua may
-    func logToFile(params: NSDictionary) -> String{
+    func logToFile(params: NSDictionary) -> String {
         let DocURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-        let file = DocURL.appendingPathComponent("demo2").appendingPathExtension("js")
+       mPathZip = DocURL
+        var file = DocURL.appendingPathComponent(mFileName).appendingPathExtension("js")
         
-        if FileManager.default.fileExists(atPath: file.path) == false{
+        //neu ten = rong hoac kich thuoc file vuot qua gioi han thi tao file moi
+        if mFileName ==  "" || checkSizeLogFile() {
+            makeFileName()
+            file = DocURL.appendingPathComponent(mFileName).appendingPathExtension("js")
+            mPathFile = file
+            if checkSizeLogFile() {
+                zipFile(password: "")
+            }
+        }
+        
+        
+        
+        if FileManager.default.fileExists(atPath: file.path) == false {
             do
             {
                 let data = try JSONSerialization.data(withJSONObject: VEventType.jsonObj, options: [])
@@ -52,12 +76,58 @@ class ConfigFunction {
 //        }catch{
 //            print(error)
 //        }
+        
+        
+       
         return DocURL.path
     }
     
-    //Ham zip file
-    func zipFile(filePath: String)  {
+    //True can phai zip, failed: van co the ghi dc
+    func checkSizeLogFile() -> Bool {
+        return true
+    }
+    
+    //ham xoa file
+    func removeFile() {
         
+    }
+    
+    //Ham zip file
+    func zipFile(password: String)  {
+        if password != "" {
+            if mPathZip != nil || mPathFile != nil{
+                let zipFilePath = mPathZip!.appendingPathComponent(mFileName + ".zip")
+                do {
+                    //                let filePath = Bundle.main.url(forResource: "file", withExtension: "zip")!
+                    //                let documentsDirectory = FileManager.default.urls(for:.documentDirectory, in: .userDomainMask)[0]
+                    //                try Zip.unzipFile(filePath, destination: documentsDirectory, overwrite: true, password: "password", progress: { (progress) -> () in
+                    //                    print(progress)
+                    //                }) // Unzip
+                    
+                    print("password = \(password)")
+                    try Zip.zipFiles(paths: [mPathFile!], zipFilePath: zipFilePath, password: "abc", progress: { (progress) -> () in
+                        print("zip progress = \(progress)")
+                    }) //Zip
+                }
+                catch {
+                    print("Something went wrong")
+                }
+            }
+        } else {
+            if mPathFile != nil{
+                do{
+                    try Zip.quickZipFiles([mPathFile!], fileName: mFileName, progress: { (progress) in
+                         print("zip progress = \(progress)")
+                        removeFile()
+                    })
+//                    try Zip.quickZipFiles([mPathFile!], fileName: mFileName
+                }
+                catch {
+                    print("Something went wrong")
+                }
+            }
+
+        }
     }
     
     // tra ve duong dan trong local may
@@ -65,15 +135,22 @@ class ConfigFunction {
         
         do {
             
-        }catch{
+        } catch{
             print(error)
         }
         return ""
     }
     
     //Ham gui zipFile len server
-    func sendFile(filePath: String) {
+    func sendFile() {
        
     }
     
+    func makeFileName() {
+        // Lay timestamp -> chuyen thanh string
+        let ticks = Int(Date().timeIntervalSince1970)
+        mFileName = String(ticks)
+        let defaults = UserDefaults.standard
+        defaults.set(mFileName, forKey: "fileName")
+    }
 }
