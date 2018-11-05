5154694ddb64293be7005eed2f6a7117b9c0468e
Nguyễn Việt <nguyenviet@VietNguyen.local>
2018-11-05 14:18:22 +0700

    update Model

--- VTAM/Model/TrackingVO.swift
+++ VTAM/Model/TrackingVO.swift
@@ -9,9 +9,73 @@
 import Foundation
 
 class TrackingVO {
-//    let trackingData : TrackingDataVO
+
+    var TrackingCode = ""
+   var  PackageId = ""
+    var RequestId = ""
+   var  DeviceInfos = [DeviceVO]()
+   var TrackingDatas = [TrackingDataVO]()
+    
+    init() {}
     
     init(data: [String: AnyObject]) {
+        TrackingCode = data["tracking-code"] as? String ?? ""
+        PackageId = data["package-id"] as? String ?? ""
+        RequestId = data["RequestId"] as? String ?? ""
         
+        guard let DeviceInfosArr = data["device-info"] as? [[String: Any]] ?? nil else {
+            print("Khong the doc")
+            return
+        }
+        
+        guard let TrackingDatasArr = data["tracking-data"] as? [[String: Any]] ?? nil else {
+            print("Khong the doc")
+            return
+        }
+        
+        
+        for item in DeviceInfosArr {
+            do {
+                let DataDeviceInfo = try  JSONSerialization.data(withJSONObject: item, options: .prettyPrinted)
+                let DataDeviceInfoJson = try JSONSerialization.jsonObject(with: DataDeviceInfo, options: .mutableLeaves)
+                let result = DataDeviceInfoJson as? [String: AnyObject] ?? nil
+                let  DeviceInfo = DeviceVO(data: result!)
+                DeviceInfos.append(DeviceInfo)
+            } catch {
+                
+            }
+            
+        }
+        
+        for item in TrackingDatasArr {
+            do {
+                let DataTrackingData = try  JSONSerialization.data(withJSONObject: item, options: .prettyPrinted)
+                let DataTrackingDataJson = try JSONSerialization.jsonObject(with: DataTrackingData, options: .mutableLeaves)
+                let result = DataTrackingDataJson as? [String: AnyObject] ?? nil
+                let  TrackingData = TrackingDataVO(data: result!)
+                //  tripnearbys.append(tripnearby)
+                TrackingDatas.append(TrackingData)
+            } catch {
+                
+            }
+            
+        }
+
+    }
+    
+    func toJsonSTring() -> [String: Any] {
+        var parameters = [String: Any]()
+        parameters["tracking-code"] = TrackingCode
+        parameters["package-id"] = PackageId
+        parameters["request-id"] = RequestId
+        parameters["device-info"] = DeviceInfos
+        parameters["tracking-data"] = TrackingDatas
+
+        return parameters
     }
 }
+//tracking-code
+//package-id
+//request-id
+//device-info
+//tracking-data
